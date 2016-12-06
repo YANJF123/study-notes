ad24a50484a2b9c28fdbdfde4ff3275baf7698dc
buuug7 <youpp@126.com>
2016-12-06 15:39:19 +0800

    add some to useful-snippet directory

--- userful-snippet/float-ad/floatAd.js
+++ userful-snippet/float-ad/floatAd.js
@@ -255,33 +255,3 @@
             $.error('漂浮广告错误!');
     };
 })(jQuery, window, document);
-
-
-// 调用方法
-$(function () {
-    $.floatingAd({
-        //频率
-        delay: 60,
-        //超链接后是否关闭漂浮
-        isLinkClosed: true,
-        //漂浮内容
-        ad: [{
-            //关闭区域背景透明度(0.1-1)
-            //headFilter: 0.1,
-            //图片
-            'img': "path/to/img.jpg",
-            //图片高度
-            'imgHeight': 50,
-            //图片宽度
-            'imgWidth': 200,
-            //图片链接
-            'linkUrl': "http://www.github.com/somelinks",
-            //浮动层级别
-            'z-index': 100,
-        },],
-        //关闭事件
-        onClose: function (elem) {
-            //alert('关闭');
-        }
-    });
-});
